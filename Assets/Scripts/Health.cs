b4f8d2ab3693e3356feab9f2d69c53fb45f638eb
Yannick Garbe <Yannickgarbe02@gmail.com>
2025-01-06 12:26:36 +0100

    XRweek1

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,34 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public float maxHealth = 100f;
+    public float currentHealth;
+    public Image healthbarFill;
+     void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateHeathBar();
+    }
+
+    void UpdateHeathBar()
+    {
+        healthbarFill.fillAmount = currentHealth / maxHealth;
+    }
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0,maxHealth);
+        UpdateHeathBar();
+    }
+    public void RestoreHealth(float amount)
+    {
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(-currentHealth, 0,maxHealth);
+        UpdateHeathBar();
+    }
+}
